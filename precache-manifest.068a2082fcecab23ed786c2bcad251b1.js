2068984908f716d5f49fa185a8d380a14e80a5b5
Dakota Madden-Fong <maddenfong@gmail.com>
2020-12-31 08:17:31 -0800

    Updates

---
+++ precache-manifest.068a2082fcecab23ed786c2bcad251b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43564638b44b72ab4ab3a9715cc3b02",
+    "url": "/mtg-guessing-game/index.html"
+  },
+  {
+    "revision": "adc4ca6d02aca0e42cc1",
+    "url": "/mtg-guessing-game/static/css/main.00c4e447.chunk.css"
+  },
+  {
+    "revision": "6afca63a541c6165a1d0",
+    "url": "/mtg-guessing-game/static/js/2.da9e665a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mtg-guessing-game/static/js/2.da9e665a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc4ca6d02aca0e42cc1",
+    "url": "/mtg-guessing-game/static/js/main.8a0bc8b5.chunk.js"
+  },
+  {
+    "revision": "90fb4c886609e31e4b5b",
+    "url": "/mtg-guessing-game/static/js/runtime-main.1500e4e4.js"
+  }
+]);
